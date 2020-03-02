728d25be3e4722edb3a4f02fa96cf32e6a8d9cef
Lyubov Olehova <lyuba@greenone.ru>
2020-03-02 22:23:02 +0300

    gulp: added task cssmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 
@@ -14,4 +16,16 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// minify css using gulp
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+ 
+gulp.task('cssmin',  (done) => {
+    gulp.src('./*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min.css'}))
+        .pipe(gulp.dest('dist'));
+    done();
 });
