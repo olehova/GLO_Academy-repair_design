b922926c5079b98b33c8ede5f67583f2967142d1
Lyubov Olehova <lyuba@greenone.ru>
2020-03-04 21:23:46 +0300

    fix all

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,46 @@
 /*jshint esversion: 6 */
 
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
+//import { src, dest, watch } from 'gulp';
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 
-gulp.task('hello', (done) => {
-  console.log("Привет, мир!");
-  done();
-});
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
+  serveLesson15Sass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./src/*.html").on('change', browserSync.reload);
+  watch("./src/sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+  watch("./lesson_15/sass/**/*.sass", serveLesson15Sass);
+}
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+  return src("./src/sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./src/css"))
+      .pipe(browserSync.stream());
+}
+
+// Compile sass into CSS & auto-inject into browsers
+function serveLesson15Sass() {
+  return src("./lesson_15/src/sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./dist/lesson_15/css"))
+      .pipe(browserSync.stream());
+}
+
+exports.serve = bs;
 
 // minify css using gulp
-const cssmin = require('gulp-cssmin');
+/* const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
  
 gulp.task('cssmin',  (done) => {
@@ -28,4 +49,4 @@ gulp.task('cssmin',  (done) => {
         .pipe(rename({suffix: '.min.css'}))
         .pipe(gulp.dest('dist'));
     done();
-});
+}); */
